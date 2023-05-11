03e353ef2dc747a6077a4d8e471e365bb77eef5f
ohv99 <114121980+ohv99@users.noreply.github.com>
2023-05-10 18:33:00 -0600

    avances

---
+++ login/lib/ui/ui.dart
@@ -0,0 +1,1 @@
+export 'package:login/ui/authInputDecoration.dart';
---
+++ login/lib/widgets/card_container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class CardContainer extends StatelessWidget {
+  final Widget child;
+  const CardContainer({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(20),
+        decoration: _createCardShape(),
+        child: this.child,
+      ),
+    );
+  }
+}
+
+BoxDecoration _createCardShape() => BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 15,
+          )
+        ]);
